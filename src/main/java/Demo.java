0600a39afae360d45030482980f6bd1bfb717ddf
NimaliW <nimaliwijesingha123@gmail.com>
2021-08-21 14:31:18 +0530

    First commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static  void main(String args[]) {
+        System.out.print("fhxvxvjxjzzjhhhh");
+    }
+
+    void test() {
+        System.out.println("will this work???//");
+    }
+}
