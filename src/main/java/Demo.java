8012045d6b15d103fcfcdf1099f9da374edb76cf
NimaliW <nimaliwijesingha123@gmail.com>
2021-08-21 14:34:37 +0530

    first commit

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,7 +2,9 @@ public class Demo {
     public static  void main(String args[]) {
         System.out.print("fhxvxvjxjzzjhhhh");
     }
-
+int a;
+    int b;
+    int c;
     void test() {
         System.out.println("will this work???//");
     }
